09e6d8e151667b6bf91772ab579f62246f2bdd4d
zaczacariah <150572949+zaczacariah@users.noreply.github.com>
2024-03-26 18:48:47 +1030

    Habitual Route get, get/:id and put

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 // Import the routes. This is how we make our routes modular.
 const goalRoutes = require('./goalRoutes');
 const actionableRoutes = require('./actionableRoutes')
-// const habitualRoutes = require('./habitualRoutes');
+const habitualRoutes = require('./habitualRoutes');
 // When a request is made to the /users or /projects path, it will be directed to the index.js in the /users or /projects folder.
 router.use('/goals', goalRoutes);
 router.use('/goals/actionable', actionableRoutes);
-// router.use('/goals/habitual', habitualRoutes);
+router.use('/goals/habitual', habitualRoutes);
 module.exports = router;
